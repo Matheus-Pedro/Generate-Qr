a1a83b7d836876d687cf6c107f76e9437709937d
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-05-02 17:40:13 -0300

    Create Generator of the QR Code 1.0

--- app/view.py
+++ app/view.py
@@ -1,27 +1,50 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import messagebox
+from tkinter import ttk as tk
+import os
+import model
+import qrcode
 
-def send(data_entry, save_entry):
-    print(data_entry, save_entry)
-
-def create_app():
+def create_app(title):
     root = Tk()
-    root.title("Gerador de QR Code")
-    frm = ttk.Frame(root, padding=10)
+    root.title(title)
+    frm = tk.Frame(root, padding=10)
     frm.grid()
 
-    ttk.Label(frm, text="Gerar um QR Code").grid(column=1, row=0)
+    tk.Label(frm, text="Para gerar o QR Code, preencha os seguintes campos.").grid(column=1, row=0)
 
-    ttk.Label(frm, text="Endereço Web:").grid(column=0, row=1)
-    data_entry = ttk.Entry(frm)
+    tk.Label(frm, text="Endereço Web:").grid(column=0, row=1)
+    data_entry = tk.Entry(frm)
     data_entry.grid(column=1, row=1)
 
-    ttk.Label(frm, text="Salvar como:").grid(column=0, row=2)
-    save_entry = ttk.Entry(frm)
+    tk.Label(frm, text="Salvar como:").grid(column=0, row=2)
+    save_entry = tk.Entry(frm)
     save_entry.grid(column=1, row=2)
 
-    ttk.Button(frm, text="Enviar", command=lambda: send(data_entry.get(), save_entry.get())).grid(column=1, row=4)
+    tk.Button(frm, text="Gerar QR", command=lambda: generate_qr(data_entry.get(), save_entry.get())).grid(column=1, row=4)
     root.mainloop()
+
+
+def generate_qr(data_address, save_entry):
+    if is_http_link(data_address):
+        object_qr = model.Qrcode(data_address=data_address, qr = qrcode.QRCode(version=1, box_size=10, border=4), save_address=f"media/qrs/{save_entry}.png")
+        object_qr.create_qr()
+    else:
+        confirmar = messagebox.askyesno("Confirmação", "Não é um link HTTP. Tem certeza que deseja prosseguir?")
+        if confirmar:
+            object_qr = model.Qrcode(data_address=data_address, qr = qrcode.QRCode(version=1, box_size=10, border=4), save_address=f"media/qrs/{save_entry}.png")
+            object_qr.create_qr()
+        else: 
+            messagebox.showinfo("Cancelado", "Operação cancelada.")
+
+
+def is_http_link(string):
+    if string.startswith("http://") or string.startswith("https://"):
+        return True
+    else:
+        return False
+
+create_app(title="Gerador de QR Code")
 
 '''
     fill: str = field(default="black")
